e733dd398a7f4301ea197905023ee33455fd7e2e
av6920337 <av6920337@gmail.com>
2024-09-09 22:50:33 +0200

    Creating a faqS block, adding styles, and js

---
+++ main.js
@@ -0,0 +1,4 @@
+
+// обработчик событий на документ/ accordion; faqS
+import{handleDocumentClick}from"./src/common";
+document.addEventListener('click', handleDocumentClick);
--- src/common.js
+++ src/common.js
@@ -1,1 +1,24 @@
 // для функций
+
+// Функции для аккордеона / faqS
+const toggleAccordionItem = (item) => {
+    item.classList.toggle('open');
+};
+
+const closeOtherAccordionItems = (currentItem) => {
+    const openItems = document.querySelectorAll('.faqS__accordion-item.open');
+    openItems.forEach((openItem) => {
+        if (openItem !== currentItem) {
+            openItem.classList.remove('open');
+        }
+    });
+};
+
+export const handleDocumentClick = (event) => {
+    if (event.target.matches('.faqS__accordion-trigger')) {
+        const item = event.target.closest('.faqS__accordion-item');
+        toggleAccordionItem(item);
+        closeOtherAccordionItems(item);
+    }
+};
+//end faqS
