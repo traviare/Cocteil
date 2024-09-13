fe02806a922a5b975c84665a315afce6b06ca188
traviare <madyka.violetta@gmail.com>
2024-09-13 19:55:02 +0300

    add js for price range

--- src/common.js
+++ src/common.js
@@ -1,5 +1,7 @@
 // для функций
 
+// Home page
+
 // Бургер меню
 import { burgerMenu } from "./vars";
 
@@ -13,7 +15,7 @@ export function closeBurgerMenu() {
   burgerMenu.classList.add("burger-menu-display");
 }
 
-//end
+// Бургер меню end
 
 // Функции для аккордеона / faqS
 const toggleAccordionItem = (item) => {
@@ -37,3 +39,54 @@ export const handleDocumentClick = (event) => {
   }
 };
 //end faqS
+
+// Home page end
+
+// Страница с каталогом товаров
+
+// Настройка ранжирования цены
+import { rangeInput, progress, priceInput } from "./vars";
+
+// заменить на суммы с json
+const priceGapMin = 1000;
+const priceGapMax = 10000;
+
+priceInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(priceInput[0].value);
+    let maxVal = parseInt(priceInput[1].value);
+
+    if (maxVal - minVal >= priceGap && maxVal <= priceGapMax) {
+      if (e.target.className === "price-range__min") {
+        rangeInput[0].value = minVal;
+        progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxVal;
+        progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+      }
+    }
+  });
+});
+
+rangeInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    //getting two ranges value and parsing them number
+    let minVal = parseInt(rangeInput[0].value);
+    let maxVal = parseInt(rangeInput[1].value);
+
+    if (maxVal - minVal < priceGapMin) {
+      if (e.target.className === "range-input__min") {
+        rangeInput[0].value = maxVal - priceGapMin;
+      } else {
+        rangeInput[1].value = maxVal + priceGapMin;
+      }
+    } else {
+      priceInput[0].value = minVal;
+      priceInput[1].value = maxVal;
+      progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
+  });
+});
+
+// Страница с каталогом товаров end
--- src/vars.js
+++ src/vars.js
@@ -12,6 +12,14 @@ export const totalPrice = document.querySelector(".total-info__total-price");
 /*end*/
 
 /*Violetta*/
+// бургер меню
 export const burgerMenuBtn = document.querySelector(".menu-btn");
 export const burgerMenu = document.querySelector(".burger-menu");
+
+// ранжирование цены
+export const rangeInput = document.querySelectorAll(".range-input input");
+export const progress = document.querySelector(".price-range__slider-progress");
+export const priceInput = document.querySelectorAll(
+  ".price-range__inputs input"
+);
 /*end*/
